e031048255e16b7405067cd7adfe418fe31760e3
Pablo Werlang <pswerlang@gmail.com>
2024-08-15 17:43:40 -0300

    Remove files from old front.

--- web/old/script/radio.js
+++
@@ -1,4 +0,0 @@
-$(document).ready( function(){
-    $('.radio').parents('label').wrap("<div class='radio'></div>");
-    $('.radio label').addClass('option').append("<div class='border'><div class='inner'></div></div>");
-});
