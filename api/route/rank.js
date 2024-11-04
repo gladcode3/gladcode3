d42c675e41ed2b1887678961af60296eb4b318eb
Luãn F <luanfernandes.ch004@academico.ifsul.edu.br>
2024-11-04 10:29:54 -0300

    GET /rank added

--- api/route/rank.js
+++ api/route/rank.js
@@ -3,13 +3,14 @@ import Rank from '../model/rank.js';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    try {
-        res.send( { "message": "Hello World! This is the ranking route." } )
+    try{
+        const query = await Rank.getRank(req.query.page, req.query.search, req.query.limit);
+        res.json(query);
 
     } catch (error) {
         const code = error.code ?? 500;
         const msg = error.message ?? "Failed to retrieve news";
-        res.status(code).send( { "code": code, "message": msg } );
+        res.status(code).send( { "message": msg } );
     }
 })
 
