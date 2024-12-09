411663763a61d2ec9b8ea541d43cf2fbf4612e0f
Pedro Souza <pedro.ads510@gmail.com>
2024-12-09 01:45:51 -0300

    ordering user list by active time

--- api/model/rank.js
+++ api/model/rank.js
@@ -1,51 +1,51 @@
-import Db from '../core/mysql.js';
-import CustomError from '../core/error.js';
-import config from '../config.js'
+// import Db from '../core/mysql.js';
+// import CustomError from '../core/error.js';
 
-export default class Rank {
 
-    static async helloWorld(){
+// export default class Rank {
+
+//     static async helloWorld(){
         
-        const sql = ("SELECT * FROM users WHERE id = ?");
-        const data = ["3930483"];
-        const query = await Db.query(sql, data);
-        if(query.length < 1){
-            return { "code": 404, "message": "Not Found." };
-        }
+//         const sql = ("SELECT * FROM users WHERE id = ?");
+//         const data = ["3930483"];
+//         const query = await Db.query(sql, data);
+//         if(query.length < 1){
+//             return { "code": 404, "message": "Not Found." };
+//         }
         
-        return { "result": query, "code": 200 };
-    }
+//         return { "result": query, "code": 200 };
+//     }
 
-    static async getRank(page, search, qnt){
-        if(!page) throw { "code": 400, "message": "Page was not sent." };
-        if(isNaN(page) || isNaN(qnt)) throw { "code": 400, "message": "Page and limit must be a number"};
+//     static async getRank(page, search, qnt){
+//         if(!page) throw { "code": 400, "message": "Page was not sent." };
+//         if(isNaN(page) || isNaN(qnt)) throw { "code": 400, "message": "Page and limit must be a number"};
 
-        let sql ;
-        let newSearch = search || ""
-        let limit = parseInt(qnt) || 10
+//         let sql ;
+//         let newSearch = search || ""
+//         let limit = parseInt(qnt) || 10
 
-        if(newSearch != ""){
-            newSearch = ` WHERE g.name LIKE '%${newSearch}%' OR u.nickname LIKE '%${newSearch}%' `;
-        }
+//         if(newSearch != ""){
+//             newSearch = ` WHERE g.name LIKE '%${newSearch}%' OR u.nickname LIKE '%${newSearch}%' `;
+//         }
         
-    try {
-        const offset = (page*limit)-limit;
+//     try {
+//         const offset = (page*limit)-limit;
 
-        sql = ` SELECT cod FROM gladiators g INNER JOIN users u on g.master = u.id ${newSearch}`;
-        const total = await Db.query(sql, []);
-        const num_rows = total.length ;
+//         sql = ` SELECT cod FROM gladiators g INNER JOIN users u on g.master = u.id ${newSearch}`;
+//         const total = await Db.query(sql, []);
+//         const num_rows = total.length ;
 
-        const sumreward = `SELECT sum(r.reward) FROM reports r INNER JOIN logs l ON l.id = r.log WHERE g.cod = r.gladiator AND l.time > CURRENT_TIME() - INTERVAL 1 DAY`;
-        const position = `SELECT count(*) FROM gladiators g2 WHERE g2.mmr >= g.mmr`;
+//         const sumreward = `SELECT sum(r.reward) FROM reports r INNER JOIN logs l ON l.id = r.log WHERE g.cod = r.gladiator AND l.time > CURRENT_TIME() - INTERVAL 1 DAY`;
+//         const position = `SELECT count(*) FROM gladiators g2 WHERE g2.mmr >= g.mmr`;
 
-        sql = ` SELECT g.name, g.mmr, u.nickname, (${sumreward}) AS sumreward, (${position}) AS position FROM gladiators g INNER JOIN users u ON g.master = u.id ${newSearch} ORDER BY g.mmr DESC limit ${limit} OFFSET ${offset}`
-        const result = await Db.query(sql, []);
-        return { "showing": offset+limit, "numRows": num_rows, "result": result };
+//         sql = ` SELECT g.name, g.mmr, u.nickname, (${sumreward}) AS sumreward, (${position}) AS position FROM gladiators g INNER JOIN users u ON g.master = u.id ${newSearch} ORDER BY g.mmr DESC limit ${limit} OFFSET ${offset}`
+//         const result = await Db.query(sql, []);
+//         return { "showing": offset+limit, "numRows": num_rows, "result": result };
 
-    } catch (error) {
-        const code = error.code ?? 500;
-        const msg = error.message ?? "Failed to retrieve Rank";
-        throw new CustomError(code, msg, error.data);
-    };
-    };
-}
+//     } catch (error) {
+//         const code = error.code ?? 500;
+//         const msg = error.message ?? "Failed to retrieve Rank";
+//         throw new CustomError(code, msg, error.data);
+//     };
+//     };
+// }
--- api/route/news.js
+++ api/route/news.js
@@ -1,49 +1,49 @@
-import express from 'express'
-import News from '../model/news.js'
-import User from '../model/users.js';
-import Auth from '../middleware/auth.js';
-const router = express.Router();
+// import express from 'express'
+// import News from '../model/news.js'
+// import User from '../model/users.js';
+// import Auth from '../middleware/auth.js';
+// const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        if(req.query.limit > 20) throw { "code": 400, "message": "Limit is too high." };
+// router.get('/', async (req, res) => {
+//     try {
+//         if(req.query.limit > 20) throw { "code": 400, "message": "Limit is too high." };
 
-        const query = await News.get(req.query.page, req.query.limit);
-        if(query.code != 200) throw query;
-        res.status(200).send(query); //Retorna em JSON
+//         const query = await News.get(req.query.page, req.query.limit);
+//         if(query.code != 200) throw query;
+//         res.status(200).send(query); //Retorna em JSON
 
-    } catch (error) {
-        const code = error.code ?? 500;
-        const msg = error.message ?? "Failed to retrieve news";
-        res.status(code).send( { "code": code, "message": msg } );
-    }
-});
+//     } catch (error) {
+//         const code = error.code ?? 500;
+//         const msg = error.message ?? "Failed to retrieve news";
+//         res.status(code).send( { "code": code, "message": msg } );
+//     }
+// });
 
-router.get('/:hash', async (req, res) => {
-    try {
-        req.optional = true;
-        const auth = await Auth.check(req, res);
-        if(auth.code === 500) throw auth;
+// router.get('/:hash', async (req, res) => {
+//     try {
+//         req.optional = true;
+//         const auth = await Auth.check(req, res);
+//         if(auth.code === 500) throw auth;
 
-        const query = await News.getByHash(req.params.hash);
-        if(query.code !== 200) throw query;
+//         const query = await News.getByHash(req.params.hash);
+//         if(query.code !== 200) throw query;
         
-        if(auth.code === 200){
-            console.log(auth)
-            const user = new User({
-                id: auth.user.id,
-                email: auth.user.email
-            });
-            const updateQuery = await user.updateReadNews();
-            if(updateQuery.code !== 200) throw updateQuery;
-        }  
-        res.status(200).send(query);
+//         if(auth.code === 200){
+//             console.log(auth)
+//             const user = new User({
+//                 id: auth.user.id,
+//                 email: auth.user.email
+//             });
+//             const updateQuery = await user.updateReadNews();
+//             if(updateQuery.code !== 200) throw updateQuery;
+//         }  
+//         res.status(200).send(query);
 
-    } catch (error) {
-        const code = error.code ?? 500;
-        const msg = error.message ?? "Failed to retrieve news";
-        res.status(code).send( { "code": code, "message": msg } );
-    };
-});
+//     } catch (error) {
+//         const code = error.code ?? 500;
+//         const msg = error.message ?? "Failed to retrieve news";
+//         res.status(code).send( { "code": code, "message": msg } );
+//     };
+// });
 
-export default router
+// export default router
