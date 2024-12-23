2fdaf9707ac9344337045343bb0e7e003ba47d52
Pedro Souza <pedro.ads510@gmail.com>
2024-12-23 16:22:20 -0300

    Testando padrão de rotas e classes (#3)
    * Organizando as variáveis do LESS.
    
    * Removendo as variáveis do Root
    
    * Refatornado o LESS do panels/_news.less
    
    * Add legacy_watch env option.
    
    * Use env var to jwt secret.
    
    * Fix some things in the User Model.
    
    * Make User models methods return the instance.
    
    * '/news' route with get requests added
    
    * Patched few issues within the Auth class
    
    * Added class 'News' with methods
    
    * Added an UTC patch for timezone handling. Patched other issues
    
    * Patched some issues within '/users' route
    
    * Sent out commits to the wrong branch. Restored it
    
    * add setup dir on less
    
    * rename profile page to 'dashboard'
    
    * Update configs for remote dev.
    
    * updating backend structure
    
    * adding users to users route names
    
    * adding gladiator and synching backend branch new models
    
    * updating error handler
    
    * changing class structure
    
    * adding getUser method to user class
    
    * creating getByName and getByMaster gladiator routes
    
    * adding getNameList to users model and route
    
    * adding dotenv configuration
    
    * implementing gladiator number by master check
    
    * adding new routes and methods to users and gladiators
    
    * filtering name list by lvl
    
    * updating error handling
    
    * ordering user list by active time
    
    * using mysql class
    
    * updating try catch error handling
    
    * adding LIKE to getByNickname query
    
    * changing speel in user by id route to not conflict user class declaration
    
    ---------
    
    Co-authored-by: Gustavo Rutkowski <rutkowskigustavo@gmail.com>
    Co-authored-by: Pablo Werlang <pswerlang@gmail.com>
    Co-authored-by: Luãn F <luanfernandes.ch004@academico.ifsul.edu.br>
    Co-authored-by: GustavoRutkowski <140657464+GustavoRutkowski@users.noreply.github.com>
    Co-authored-by: werlang-server <werlang@server>
    Co-authored-by: Luãn Fernandes <147353547+Konataware@users.noreply.github.com>

--- api/model/users.js
+++ api/model/users.js
@@ -1,180 +1,201 @@
-import CustomError from '../core/error.js';
-import Db from '../core/mysql.js';
+import CustomError from "../core/error.js";
+import db from "../core/mysql.js";
 
 export default class User {
+  constructor({
+    id,
+    email,
+    googleid,
+    firstName,
+    lastName,
+    nickname,
+    profilePicture,
+  }) {
+    this.id = id;
+    this.email = email;
+    this.googleid = googleid;
+    this.nickname = nickname;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.profilePicture = profilePicture || null;
+  }
 
-    // id, email, googleid, nickname, first_name, last_name, profile_picture, spoken_language, emoji
-    // pasta, lvl, xp, silver, credits, active, premium, show_tutorial, editor_theme, editor_font
-    // pref_message, pref_friend, pref_update, pref_duel, pref_tourn, email_update, read_news
-    // pref_language, apothecary
+  async delete() {
+    return db.delete("users", this.id);
+  }
 
-    //Snake casing para atributos associados com o MySQL
-    constructor({ id, email, googleid, nickname, first_name, last_name, profile_picture, pasta, pref_language }) {
-        this.id = id,
-        this.email = email,
-        this.googleid = googleid,
-        this.nickname = nickname,
-        this.first_name = first_name,
-        this.last_name = last_name,
-        this.profile_picture = profile_picture,
-        this.pasta = pasta;
-        this.pref_language = pref_language
-    };
-  
-    async get() {
-        const user = await Db.find('users', {
-            filter: { id: this.id },
-            view: ['email', 'nickname', 'first_name', 'last_name', 'profile_picture']
-        });
-        if (user.length === 0) throw new CustomError(404, `User does not exist`);
-        return user[0];
-    };
+  async add() {
+    try {
+      const sql = await db.insert("users", {
+        email: this.email,
+        googleid: this.googleid,
+        nickname: this.nickname,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        profilePicture: this.profilePicture,
+        active: db.toDateTime(Date.now()),
+      });
 
-    static async getNameList(name){
-        if (!name) throw new CustomError(400, 'Nickname is required');
-
-        const results = [];
-        const exact = await Db.find('users', {
-            filter: { nickname: name },
-            view: [ 'nickname', 'profile_picture' ],
-            opt: { limit: 1 }
-        });
-        if(exact.length === 1) results.push(exact[0]);
-
-        const prefix = await Db.find('users', {
-            filter: { nickname: Db.like( `${name}_%` ) },
-            view: [ 'nickname', 'profile_picture' ],
-            opt: { limit: 5 }
-        });
-        if(prefix.length >= 1) {
-            prefix.forEach(query => {
-                results.push(query);
-            });
-        };
-        
-        const suffix = await Db.find('users', {
-            filter: { nickname: Db.like( `%_${name}` ) },
-            view: ['nickname', 'profile_picture'],
-            opt: { limit: 5 }
-        });
-        if(suffix.length >= 1) {
-            suffix.forEach(query => {
-                results.push(query);
-            });
-        };
-
-        if(results.length === 0) throw new CustomError(404, "No results found.");
-        return { "results": results };
-    };
-              
-    async delete(){
-        const find = await Db.find('users', {
-            filter: { id: this.id },
-            view: [ 'id' ],
-            opt: { limit: 1 }
-        });
-        if(find.length === 0) throw new CustomError(404, "User not found.")
-        await Db.delete('users', this.id);
-
-        return { "message": "User has been deleted successfully." };
-    };
-
-    async add(){
-        const activeTime = await utcFix();
-        const sql = await Db.insert('users',
-        {
-            email: `${this.email}`,
-            googleid: `${this.googleid}`,
-            nickname: `${this.nickname}`,
-            first_name: `${this.first_name}`,
-            last_name: `${this.last_name}`,
-            profile_picture: `${this.profile_picture}`,
-            active: `${activeTime}`,
-            pasta: `${this.pasta}`
-        });
-
-        this.id = sql[0].insertId;
-        return { "User": this.get(), "code": 200 };
-    };
-
-    async update() {
-        if (this.nickname !== undefined) await Db.update('users', { nickname: this.nickname }, this.id);
-        if (this.profile_picture !== undefined) await Db.update('users', { profile_picture: this.profile_picture }, this.id);
-        if (this.pref_language !== undefined) await Db.update('users', { pref_language: this.pref_language }, this.id);
-        return this.get();
+      this.id = sql[0].insertId;
+      return this.get();
+    } catch (error) {
+      throw new CustomError(
+        error.code ?? 500,
+        error.message ?? "Internal server error"
+      );
     }
+  }
 
-    async loginUser(){
-        try {
-            if(!this.pasta) throw new CustomError(400, 'Password is required');
-        
-            const idCheck = await Db.find('users', {
-                filter: { id: Db.like(this.id) },
-                view: ['email'],
-                opt: { limit: 1}
-            });
-            if(idCheck.length === 0) throw new CustomError(404, `User ID not found`);
-    
-            const pswd = await Db.find('users', {
-                filter: { id: `${this.id}`}, 
-                view: ['pasta'] 
-            });
-            if(pswd.length === 0 || this.pasta !== pswd[0].pasta) throw new CustomError(401, `Wrong username or password`);
-            
-            const userPayload = {
-                id: `${this.id}`
-            }
-            return jwt.sign(userPayload, process.env.SIGN_TOKEN);
+  async getByNickname(nickname) {
+    if (!nickname) throw new CustomError(400, "Nickname is required");
+    try {
+      const users = await db.find("users", {
+        filter: { nickname: { like: nickname } },
+        opt: { limit: 20 },
+      });
 
-        } catch (error) {
-            const code = error.code ?? 500;
-            const msg = error.message ?? "Failed to login."
-            return new CustomError(code, msg);
-        };
-    };
+      return users;
+    } catch (error) {
+      throw new CustomError(
+        error.code ?? 500,
+        error.message ?? "Internal server error"
+      );
+    }
+  }
 
-    async updateReadNews(){
-        const activeTime = await utcFix();
-        await Db.update('users', { read_news: activeTime }, this.id);
-        return;
-    };
+  async getById(id) {
+    if (!id) throw new CustomError(400, "ID is required");
 
-    async updateActive(){
-        const activeTime = await utcFix();
-        await Db.update('users', { active: activeTime }, this.id);
-        return;
-    };
+    try {
+      const users = await db.find("users", {
+        filter: { id },
+      });
 
-    static async fetchData(filter, value){
-        if(filter === "email" && typeof(value) === "string"){
-            try {
-                const id = await Db.find('users', { filter: { email: value }, view: [ 'id' ], opt: { limit: 1 } });
-                if(id.length === 0) throw { "code": 404, "message": "User not Found" };
-                return { "code": 200, "id": id[0].id };
+      return users;
+    } catch (error) {
+      throw new CustomError(
+        error.code ?? 500,
+        error.message ?? "Internal server error"
+      );
+    }
+  }
 
-            } catch (error) {
-                const code = error.code ?? 500;
-                const msg = error.message ?? "Internal Auth Issues";
-                return new CustomError(code, msg);
-            };
+  async update() {
+    try {
+      if (this.nickname !== undefined && this.nickname !== null) {
+        console.log(`Updating nickname: ${this.nickname}`);
+        await db.update("users", { nickname: this.nickname }, this.id);
+      }
 
-        } else if(filter === "id"){
-            try {
-                const email = await Db.find('users', { filter:  { id: value }, view: [ 'email' ], opt: { limit: 1 } });
-                if(email.length === 0) throw { "code": 404, "message": "User not Found" };
-                return { "code": 200, "email": email[0].email };
+      if (this.firstName !== undefined && this.firstName !== null) {
+        console.log(`Updating firstName: ${this.firstName}`);
+        await db.update("users", { firstName: this.firstName }, this.id);
+      }
 
-            } catch (error) {
-                const code = error.code ?? 500;
-                const msg = error.message ?? "Internal Server Issues";
-                return new CustomError(code, msg);
-            };
-        };
-    };
-};
+      if (this.lastName !== undefined && this.lastName !== null) {
+        console.log(`Updating lastName: ${this.lastName}`);
+        await db.update("users", { lastName: this.lastName }, this.id);
+      }
+
+      if (this.email !== undefined && this.email !== null) {
+        console.log(`Updating email: ${this.email}`);
+        await db.update("users", { email: this.email }, this.id);
+      }
+      return this.get();
+    } catch (error) {
+      console.error(`Error in update: ${error.message}`);
+      throw new CustomError(500, "Internal server error");
+    }
+  }
+
+  async getNameList() {
+    try {
+      const users = await db.find("users", {
+        opt: {
+          order: {
+            active: -1,
+          },
+          limit: 20,
+        },
+      });
+
+      return users;
+    } catch (error) {
+      throw new CustomError(
+        error.code ?? 500,
+        error.message ?? "Internal server error"
+      );
+    }
+  }
+
+  async get() {
+    try {
+      const user = await db.find("users", {
+        filter: { id: this.id },
+        view: ["email", "nickname", "firstName", "lastName", "profilePicture"],
+      });
+
+      if (user.length === 0) throw new CustomError(404, "User does not exist");
+      return user[0];
+    } catch (error) {
+      throw new CustomError(
+        error.code ?? 500,
+        error.message ?? "Internal server error"
+      );
+    }
+  }
+
+  async updateReadNews() {
+    const activeTime = await utcFix();
+    await db.update("users", { read_news: activeTime }, this.id);
+  }
+
+  async updateActive() {
+    const activeTime = await utcFix();
+    await db.update("users", { active: activeTime }, this.id);
+  }
+
+  static async fetchData(filter, value) {
+    if (filter === "email" && typeof value === "string") {
+      try {
+        const id = await db.find("users", {
+          filter: { email: value },
+          view: ["id"],
+          opt: { limit: 1 },
+        });
+
+        if (id.length === 0)
+          throw new CustomError(404, "User not Found");
+
+        return { code: 200, id: id[0].id };
+      } catch (error) {
+        const code = error.code ?? 500;
+        const msg = error.message ?? "Internal Auth Issues";
+        throw new CustomError(code, msg);
+      }
+    } else if (filter === "id") {
+      try {
+        const email = await db.find("users", {
+          filter: { id: value },
+          view: ["email"],
+          opt: { limit: 1 },
+        });
+
+        if (email.length === 0)
+          throw new CustomError(404, "User not Found");
+
+        return { code: 200, email: email[0].email };
+      } catch (error) {
+        const code = error.code ?? 500;
+        const msg = error.message ?? "Internal Server Issues";
+        throw new CustomError(code, msg);
+      }
+    }
+  }
+}
 
 async function utcFix() {
-    const now = Date.now();
-    const utcfix = now - (3 * 60 * 60 * 1000); 
-    return Db.toDateTime(utcfix); 
-};
+  const now = Date.now();
+  const utcfix = now - 3 * 60 * 60 * 1000;
+  return db.toDateTime(utcfix);
+}
